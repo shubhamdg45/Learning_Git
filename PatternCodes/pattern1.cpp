61576631ff259b82c3ae2d91ed533cdd6db9fee3
shuham Giram <shubhamg2023@gmail.com>
2024-02-16 01:30:47 +0530

    fist commit

---
+++ PatternCodes/pattern1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+/*
+ * 1  2  3  4  5 
+ * 6  7  8  9  10
+ * 11 12 13 14 15
+ * 16 17 18 19 20
+ * 21 22 23 24 25
+ */
+int main(){
+	int count=1;
+	for(int i=1;i<=5;i++)
+	{
+	for(int j=1;j<=5;j++){
+		cout<<count<<" ";
+		count++;
+
+	}
+	cout << "Hello World";
+cout<<endl;
+	}
+	return 0;
+}
+/*
+
+50----
+
+*/
